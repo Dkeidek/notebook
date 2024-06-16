98e7dd95e4a10b8324b8c7866303b5067a5077e2
mohdk <Mohammad.Dkeidek@Student.HTW-Berlin.de>
2024-06-16 05:40:13 +0200

    edit,delete update

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="notes">
-
     <div class="card has-background-success-white p-4 mb-5">
       <div class="field">
         <div class="control">
@@ -11,19 +10,18 @@
           />
         </div>
       </div>
-
       <div class="field is-grouped is-grouped-right">
         <div class="control">
-          <button 
-          @click="addNote"
-          :disabled="!newNote"
-          class="button is-link has-background-success">
+          <button
+              @click="addNote"
+              :disabled="!newNote"
+              class="button is-link has-background-success"
+          >
             Add New Note
           </button>
         </div>
       </div>
     </div>
-
     <div
         v-for="item in items"
         :key="item.id"
@@ -31,12 +29,12 @@
     >
       <div class="card-content">
         <div class="content">
-         {{ item.description  }}
+          {{ item.description }}
         </div>
       </div>
       <footer class="card-footer">
-        <a href="#" class="card-footer-item">Edit</a>
-        <a href="#" class="card-footer-item">Delete</a>
+        <a href="#" class="card-footer-item" @click="editNote(item)">Edit</a>
+        <a href="#" class="card-footer-item" @click="deleteNote(item.id)">Delete</a>
       </footer>
     </div>
   </div>
@@ -46,26 +44,51 @@
 import { ref, onMounted } from 'vue';
 import apiClient from '../axios';
 
-
 const items = ref([]);
 const newNote = ref('');
 
-const addNote =  async ()=>{
-  const response = await apiClient.post('/notebook',{
-     description: newNote.value,
-      owner: "mohammad"
-  });
-  newNote.value = ''
-}
 const fetchData = async () => {
   try {
-    const response = await apiClient.get('/notebook'); // Get
-    items.value = response.data; //json
-    console.log(response.data)
+    const response = await apiClient.get('/notebook');
+    items.value = response.data;
   } catch (error) {
     console.error('Error fetching data:', error);
   }
 };
 
+const addNote = async () => {
+  try {
+    const response = await apiClient.post('/notebook', {
+      description: newNote.value,
+      owner: 'mohammad'
+    });
+    newNote.value = '';
+    fetchData();
+  } catch (error) {
+    console.error('Error adding note:', error);
+  }
+};
+
+const editNote = (item) => {
+  const updatedDescription = prompt('Edit your note', item.description);
+  if (updatedDescription !== null) {
+    apiClient.put(`/notebook/${item.id}`, {
+      ...item,
+      description: updatedDescription
+    }).then(fetchData).catch(error => {
+      console.error('Error updating note:', error);
+    });
+  }
+};
+
+const deleteNote = (id) => {
+  apiClient.delete(`/notebook/${id}`)
+      .then(fetchData)
+      .catch(error => {
+        console.error('Error deleting note:', error);
+      });
+};
+
 onMounted(fetchData);
 </script>
+
